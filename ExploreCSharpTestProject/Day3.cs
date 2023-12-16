9d1168890acf7265a1b3c5d259b099de0443c32f
Boris Blokland <boris.blokland@alten.nl>
2023-12-16 15:42:46 +0100

    WIP day 3

--- ExploreCSharpTestProject/Day3.cs
+++ ExploreCSharpTestProject/Day3.cs
@@ -13,15 +13,45 @@ public class EngineNumbers
 
     public int CalculateNumber()
     {
-        for (int x = 0; x < input.Length; x++) {
-            int start_number = -1;
-            for (int y = 0; y < input[x].Length; y++) {
-                if (Char.IsDigit(input[x][y])) {
-                    
-                }
+        for (int y = 0; y < input.Length; y++) {
+            lineLength = input[y].Length;
+            for (int x = 0; x < lineLength; x++) {
+                ProcessCharacter(x, y);                
             }
         }
         return 0;
+    }
+    private int lineLength = 0;
+
+    private bool numberActive = false;
+    private bool isNumberWithSymbol = false;
+    private int currentNumber = 0;
+
+    private void ProcessCharacter(int x, int y) {
+        char character = input[x][y];
+        if (Char.IsDigit(character)) {
+            int value = int.Parse(character.ToString());
+            if (numberActive) {
+                currentNumber = currentNumber * 10 + value;
+            } else {
+                numberActive = true;
+                currentNumber = value;
+            }
+            if (!isNumberWithSymbol) {
+                isNumberWithSymbol = CheckForSymbolNeighbour(x, y);
+            }
+        } else {
+            numberActive = false;
+            isNumberWithSymbol = false;
+            currentNumber = 0;
+        }
+    }
+
+    private bool CheckForSymbolNeighbour(int x, int y) {
+        int nextIndex = x + 1;
+        if (nextIndex < lineLength) {
+            char nextChar = input[nextIndex][y];
+        }
     }
 }
 
