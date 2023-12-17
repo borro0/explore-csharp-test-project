7be4f4685b03ebc44b157a8cfe029a831c1a5b86
Boris Blokland <boris.blokland@alten.nl>
2023-12-17 14:02:05 +0100

    almost working day 3 solution

--- ExploreCSharpTestProject/Day3.cs
+++ ExploreCSharpTestProject/Day3.cs
@@ -3,6 +3,18 @@ using System.IO;
 using Xunit.Sdk;
 using System.Reflection;
 
+public struct Position
+{
+    public int CharIndex;
+    public int LineIndex;
+
+    public Position(int charIndex, int lineIndex)
+    {
+        CharIndex = charIndex;
+        LineIndex = lineIndex;
+    }
+}
+
 public class EngineNumbers
 {
     private string[] input;
@@ -13,56 +25,184 @@ public class EngineNumbers
 
     public int CalculateNumber()
     {
-        for (int y = 0; y < input.Length; y++) {
-            lineLength = input[y].Length;
-            for (int x = 0; x < lineLength; x++) {
-                ProcessCharacter(x, y);                
+        for (int lineIndex = 0; lineIndex < input.Length; lineIndex++)
+        {
+            lineLength = input[lineIndex].Length;
+            for (int charIndex = 0; charIndex < lineLength; charIndex++)
+            {
+                ProcessCharacter(charIndex, lineIndex);
             }
         }
-        return 0;
+        return sum;
     }
     private int lineLength = 0;
 
     private bool numberActive = false;
     private bool isNumberWithSymbol = false;
     private int currentNumber = 0;
+    private int sum = 0;
 
-    private void ProcessCharacter(int x, int y) {
-        char character = input[x][y];
-        if (Char.IsDigit(character)) {
-            int value = int.Parse(character.ToString());
-            if (numberActive) {
-                currentNumber = currentNumber * 10 + value;
-            } else {
-                numberActive = true;
-                currentNumber = value;
-            }
-            if (!isNumberWithSymbol) {
-                isNumberWithSymbol = CheckForSymbolNeighbour(x, y);
-            }
-        } else {
-            numberActive = false;
-            isNumberWithSymbol = false;
-            currentNumber = 0;
+    private void ProcessCharacter(int charIndex, int lineIndex)
+    {
+        char character = input[lineIndex][charIndex];
+        if (Char.IsDigit(character))
+        {
+            ProcessDigit(charIndex, lineIndex, character);
+        }
+        else
+        {
+            ProcessNonDigit();
         }
     }
 
-    private bool CheckForSymbolNeighbour(int x, int y) {
-        int nextIndex = x + 1;
-        if (nextIndex < lineLength) {
-            char nextChar = input[nextIndex][y];
+    private void ProcessNonDigit()
+    {
+        if (numberActive)
+        {
+            CloseNumber();
         }
+
+    }
+
+    private void CloseNumber()
+    {
+        if (isNumberWithSymbol)
+        {
+            sum += currentNumber;
+        }
+        numberActive = false;
+        isNumberWithSymbol = false;
+        currentNumber = 0;
+    }
+
+    private void ProcessDigit(int charIndex, int lineIndex, char character)
+    {
+        int value = int.Parse(character.ToString());
+        if (numberActive)
+        {
+            currentNumber = currentNumber * 10 + value;
+        }
+        else
+        {
+            numberActive = true;
+            currentNumber = value;
+        }
+        if (!isNumberWithSymbol)
+        {
+            isNumberWithSymbol = CheckForSymbolNeighbor(charIndex, lineIndex);
+        }
+    }
+
+    private bool CheckForSymbolNeighbor(int charIndex, int lineIndex)
+    {
+        List<Position> positionsToCheck = new List<Position>();
+        positionsToCheck.Add(new Position(charIndex - 1, lineIndex));
+        positionsToCheck.Add(new Position(charIndex + 1, lineIndex));
+        positionsToCheck.Add(new Position(charIndex - 1, lineIndex + 1));
+        positionsToCheck.Add(new Position(charIndex, lineIndex + 1));
+        positionsToCheck.Add(new Position(charIndex + 1, lineIndex + 1));
+        foreach (Position position in positionsToCheck)
+        {
+            if (IsPositionValid(position) && IsSymbolAtPosition(position))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool IsPositionValid(Position position)
+    {
+        bool validCharIndex = position.CharIndex >= 0 && position.CharIndex < lineLength;
+        bool validLineIndex = position.LineIndex >= 0 && position.LineIndex < input.Length;
+        return validCharIndex && validLineIndex;
+    }
+
+    private bool IsSymbolAtPosition(Position position)
+    {
+        char nextChar = input[position.LineIndex][position.CharIndex];
+        return IsSymbol(nextChar);
+    }
+
+    private static bool IsSymbol(char character)
+    {
+        if (Char.IsDigit(character))
+        {
+            return false;
+        }
+        if (character == '.')
+        {
+            return false;
+        }
+        return true;
     }
 }
 
 public class Day3Test
 {
     [Fact]
-    public void Test_SingleLine()
+    public void Test_SingleLineValidNumber()
     {
         string[] input = { "467*.." };
         EngineNumbers engine = new EngineNumbers(input);
         Assert.Equal(467, engine.CalculateNumber());
+    }
+
+    [Fact]
+    public void Test_SingleLineInvalidNumber()
+    {
+        string[] input = { "467.*." };
+        EngineNumbers engine = new EngineNumbers(input);
+        Assert.Equal(0, engine.CalculateNumber());
+    }
+
+    [Fact]
+    public void Test_SingleLineSymbolAtStart()
+    {
+        string[] input = { "..*18.." };
+        EngineNumbers engine = new EngineNumbers(input);
+        Assert.Equal(18, engine.CalculateNumber());
+    }
+
+    [Fact]
+    public void Test_SingleLineMultipleNumbers()
+    {
+        string[] input = { "..*18..345.45).1.12!" };
+        EngineNumbers engine = new EngineNumbers(input);
+        Assert.Equal(18 + 45 + 12, engine.CalculateNumber());
+    }
+
+    [Fact]
+    public void Test_MultipleLines_LineBelowRightBottom()
+    {
+        string[] input = {
+            ".3..",
+            "..*."
+        };
+        EngineNumbers engine = new EngineNumbers(input);
+        Assert.Equal(3, engine.CalculateNumber());
+    }
+
+    [Fact]
+    public void Test_MultipleLines_LineBelowMiddle()
+    {
+        string[] input = {
+            ".3..",
+            ".*.."
+        };
+        EngineNumbers engine = new EngineNumbers(input);
+        Assert.Equal(3, engine.CalculateNumber());
+    }
+
+    [Fact]
+    public void Test_MultipleLines_LineBelowLeftBottom()
+    {
+        string[] input = {
+            ".3..",
+            "*..."
+        };
+        EngineNumbers engine = new EngineNumbers(input);
+        Assert.Equal(3, engine.CalculateNumber());
     }
 
 }
