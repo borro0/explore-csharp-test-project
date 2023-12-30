7fc7b1cc821ffb8fc97fe90d9fea0793b3c99aae
Boris Blokland <boris.blokland@alten.nl>
2023-12-30 14:54:37 +0100

    Finished day 5 part 1

--- ExploreCSharpTestProject/day5/Day5Test.cs
+++ ExploreCSharpTestProject/day5/Day5Test.cs
@@ -6,14 +6,76 @@ public class Day5Test
     public void Test_SeedToSoilMap()
     {
         string[] input = {
-            "seeds: 79 14 55 13",
+            "seeds: 79 55",
             "",
             "seed-to-soil map:",
             "50 98 2",
             "52 50 48",
         };
         SeedMapper seedMapper = new SeedMapper(input);
-        int result = seedMapper.GetLowestMappedSeed();
+        long result = seedMapper.GetLowestMappedSeed();
+        Assert.Equal(57, result);
+    }
+
+    [Fact]
+    public void Test_SeedToSoilMapNotInMap()
+    {
+        string[] input = {
+            "seeds: 79 55 14 13",
+            "",
+            "seed-to-soil map:",
+            "50 98 2",
+            "52 50 48",
+        };
+        SeedMapper seedMapper = new SeedMapper(input);
+        long result = seedMapper.GetLowestMappedSeed();
         Assert.Equal(13, result);
+    }
+
+    [Fact]
+    public void Test_SeedToSoilMapBiggerList()
+    {
+        string[] input = {
+            "seeds: 79 55",
+            "",
+            "seed-to-soil map:",
+            "1 1 1",
+            "50 98 2",
+            "52 50 48",
+        };
+        SeedMapper seedMapper = new SeedMapper(input);
+        long result = seedMapper.GetLowestMappedSeed();
+        Assert.Equal(57, result);
+    }
+
+    [Fact]
+    public void Test_MultipleMaps()
+    {
+        string[] input = {
+            "seeds: 79 14 55 13",
+            "",
+            "seed-to-soil map:",
+            "50 98 2",
+            "52 50 48",
+            "",
+            "soil-to-fertilizer map:",
+            "0 15 37",
+            "37 52 2",
+            "39 0 15",
+        };
+        SeedMapper seedMapper = new SeedMapper(input);
+        long result = seedMapper.GetLowestMappedSeed();
+        Assert.Equal(52, result);
+    }
+
+
+    [Fact]
+    public void Test_CompleteSolution()
+    {
+        string input_file_path = @"C:\Users\bblokland\explore-csharp-test-project\ExploreCSharpTestProject\day5\input.txt";
+        string[] input = File.ReadAllLines(input_file_path);
+        SeedMapper seedMapper = new SeedMapper(input);
+        long result = seedMapper.GetLowestMappedSeed();
+        Console.WriteLine($"result: {result}");
     }
 }
--- ExploreCSharpTestProject/day5/SeedMapper.cs
+++ ExploreCSharpTestProject/day5/SeedMapper.cs
@@ -8,19 +8,70 @@ public class SeedMapper
         this.input = input;
     }
 
-    public int GetLowestMappedSeed()
+    public long GetLowestMappedSeed()
     {
+        // seeds: 79 55
+        // 
+        // seed-to-soil map:
+        // 50 98 2
+        // 52 50 48
         string[] seeds_parts = input[0].Split(": ");
-        List<int> seeds = ParseNumbers(seeds_parts[1]);
+        List<long> seeds = ParseNumbers(seeds_parts[1]);
+        List<string> mapString = new List<string>();
+        bool isMapActive = false;
+
+        for (int i = 2; i < input.Length; i++) {
+            if (input[i].Contains("map")) {
+            } else if (input[i].Length == 0) {
+                seeds = MapSeeds(seeds, mapString);
+                mapString.Clear();
+                isMapActive = false;
+            } else {
+                mapString.Add(input[i]);
+                isMapActive = true;
+            }
+        }
+
+        if (isMapActive) {
+            seeds = MapSeeds(seeds, mapString);
+        }
+
+        return seeds.Min();
     }
 
-    public List<int> ParseNumbers(string input)
+    public List<long> MapSeeds(List<long> seeds, List<string> map_strings) {
+        List<List<long>> mapFunctions = new List<List<long>>();
+        List<long> mappedValues = new List<long>();
+        foreach(string map in map_strings) {
+            mapFunctions.Add(ParseNumbers(map));
+        }
+        foreach (long seed in seeds) {
+            bool isMapped = false;
+            foreach(List<long> map_function in mapFunctions) {
+                long source_start = map_function[1];
+                long range = map_function[2];
+                if (seed >= source_start && seed <= source_start + range) {
+                    long destination_start = map_function[0];
+                    long mapped_value = seed - source_start + destination_start;
+                    mappedValues.Add(mapped_value);
+                    isMapped = true;
+                    break;
+                }
+            }
+            if (!isMapped) {
+                mappedValues.Add(seed);
+            }
+        }
+        return mappedValues;
+    }
+
+    public List<long> ParseNumbers(string input)
     {
-        List<int> numbers = new List<int>();
+        List<long> numbers = new List<long>();
         foreach (string number in input.Split(" "))
         {
-            int number_value;
-            if (int.TryParse(number, out number_value))
+            long number_value;
+            if (long.TryParse(number, out number_value))
             {
                 numbers.Add(number_value);
             }
